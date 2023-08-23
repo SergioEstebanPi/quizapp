e5fb91029409af795a890c326eec37e6e8d10db9
spina <spina@MCEX06X39.corporacionbci.cl>
2023-08-23 00:42:12 -0500

    Obtengo los registros de la bd

---
+++ src/main/java/com/example/quizapp/service/QuestionService.java
@@ -0,0 +1,19 @@
+package com.example.quizapp.service;
+
+import com.example.quizapp.Question;
+import com.example.quizapp.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    private QuestionDao questionDao;
+
+    public List<Question> getAllQuestions(){
+        return questionDao.findAll();
+    }
+}
