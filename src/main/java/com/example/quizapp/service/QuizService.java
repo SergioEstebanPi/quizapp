49d0345667f5ffa401a791033eee65c4a3e89b13
spina <spina@MCEX06X39.corporacionbci.cl>
2023-08-23 10:56:22 -0500

    Agrego endpoint para obtener las preguntas del quiz

--- src/main/java/com/example/quizapp/service/QuizService.java
+++ src/main/java/com/example/quizapp/service/QuizService.java
@@ -3,6 +3,7 @@ package com.example.quizapp.service;
 import com.example.quizapp.dao.QuestionDao;
 import com.example.quizapp.dao.QuizDao;
 import com.example.quizapp.model.Question;
+import com.example.quizapp.model.QuestionWrapper;
 import com.example.quizapp.model.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -30,5 +32,20 @@ public class QuizService {
         quizDao.save(quiz);
 
         return new ResponseEntity<>("success", HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Question> questionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+        for(Question question : questionsFromDB){
+            QuestionWrapper questionWrapper = new QuestionWrapper(question.getId(),
+                    question.getQuestionTitle(),
+                    question.getOption1(),
+                    question.getOption2(),
+                    question.getOption3());
+            questionsForUser.add(questionWrapper);
+        }
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
     }
 }
